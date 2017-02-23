78cc28f3921cb658688ee820c357bba389ae8536
jerza1 <fco.j.bautista@hotmail.com>
2017-02-23 00:33:16 -0600

    Add files via upload

---
+++ GenerarAutomatas/src/generarautomatas/GenerarAutomatas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generarautomatas;
+
+/**
+ *
+ * @author jerza
+ */
+public class GenerarAutomatas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
